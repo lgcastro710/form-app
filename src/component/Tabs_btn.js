b10edcc42cb63d5c57e656ddb43a005d017106f0
Carballo <jczord23@gmail.com>
2020-11-19 05:45:45 -0600

    exportaciones por defecto

--- src/component/Tabs_btn.js
+++ src/component/Tabs_btn.js
@@ -5,8 +5,8 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Form1 from "./Form1";
-import Form2 from "./Form2";
+import {Form1} from "./Form1";
+import {Form2} from "./Form2";
 import { useState } from 'react';
 
 function TabPanel(props) {
